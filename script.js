ca390837edd811e6ddce6747daea83f371070be6
arvinantobahtiar <arvinantobahtiar@gmail.com>
2023-11-30 00:31:56 +0700

    f

--- script.js
+++ script.js
@@ -7,49 +7,49 @@ const form = document.getElementById("form");
 const search = document.getElementById("query");
 
 returnMovies(APILINK)
-function returnMovies(url) {
-    fetch(url).then(res=> res.json())
-    .then(function(data) {
-        console.log(data.results);
-        data.results.forEach(element => {
-            const div_card = document.createElement('div');
-            div_card.setAttribute('class', 'card');
+    function returnMovies(url) {
+        fetch(url).then(res=> res.json())
+        .then(function(data) {
+            console.log(data.results);
+            data.results.forEach(element => {
+                const div_card = document.createElement('div');
+                div_card.setAttribute('class', 'card');
 
-            const div_row = document.createElement('div');
-            div_row.setAttribute('class', 'row');
+                const div_row = document.createElement('div');
+                div_row.setAttribute('class', 'row');
 
-            const div_column = document.createElement('div');
-            div_column.setAttribute('class', 'column');
+                const div_column = document.createElement('div');
+                div_column.setAttribute('class', 'column');
 
-            const thumbnail = document.createElement('img');
-            thumbnail.setAttribute('class', 'thumbnail');
-            thumbnail.setAttribute('id', 'image');
+                const thumbnail = document.createElement('img');
+                thumbnail.setAttribute('class', 'thumbnail');
+                thumbnail.setAttribute('id', 'image');
 
-            const title = document.createElement('h3');
-            title.setAttribute('id', 'title');
+                const title = document.createElement('h3');
+                title.setAttribute('id', 'title');
 
-            const rating = document.createElement('h5');
-            rating.setAttribute('id', 'rating');
-            let star_emoji = emojione.toShort("⭐"); 
+                const rating = document.createElement('h5');
+                rating.setAttribute('id', 'rating');
+                let star_emoji = emojione.toShort("⭐"); 
 
-            const center = document.createElement('center');
+                const center = document.createElement('center');
 
-            title.innerHTML = `${element.title}`;
-            thumbnail.src = `${IMG_PATH + element.poster_path}`;
-            rating.innerHTML = `${element.vote_average}`;
+                title.innerHTML = `${element.title}`;
+                thumbnail.src = `${IMG_PATH + element.poster_path}`;
+                rating.innerHTML = `${star_emoji+element.vote_average}`;
 
-            center.appendChild(thumbnail);
-            div_card.appendChild(center);
-            div_card.appendChild(title);
-            div_card.appendChild(rating);
-            div_column.appendChild(div_card);
-            div_row.appendChild(div_column);
+                center.appendChild(thumbnail);
+                div_card.appendChild(center);
+                div_card.appendChild(title);
+                div_card.appendChild(rating);
+                div_column.appendChild(div_card);
+                div_row.appendChild(div_column);
 
-            main.appendChild(div_row);
+                main.appendChild(div_row);
 
+            });
         });
-    });
-}
+    }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
